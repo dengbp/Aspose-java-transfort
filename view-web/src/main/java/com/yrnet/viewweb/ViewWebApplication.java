8a31d226b750b9aba710726e763ec98ac61c1b64
dengbp <siqing0822@163.com>
2022-03-14 15:20:21 +0800

    Initial commit

---
+++ view-web/src/main/java/com/yrnet/viewweb/ViewWebApplication.java
@@ -0,0 +1,13 @@
+package com.yrnet.viewweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewWebApplication.class, args);
+    }
+
+}
---
+++ view-web/src/test/java/com/yrnet/viewweb/ViewWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrnet.viewweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
